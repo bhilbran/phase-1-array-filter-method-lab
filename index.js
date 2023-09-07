17ecbc8a3e6f3b1fd34ea3def1022dd62d31470a
brooke <bhilbran@asu.edu>
2023-09-07 14:03:07 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    const lowercaseString = string.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === lowercaseString);
+    return matchingDrivers;
+  }
+  function fuzzyMatch(drivers, query) {
+    let matchedDrivers = [];
+  
+    for (let i = 0; i < drivers.length; i++) {
+      if (drivers[i].startsWith(query)) {
+        matchedDrivers.push(drivers[i]);
+      }
+    }
+  
+    return matchedDrivers;
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
